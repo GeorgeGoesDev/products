f994bef9b8df5613a9caf79ddb89005c2bed9f99
GeorgeGoesDev <g.koutanis@gmail.com>
2023-03-23 09:26:47 +0100

    mongo integr

--- my-products/src/app/api/products/route.ts
+++ my-products/src/app/api/products/route.ts
@@ -1,10 +1,11 @@
 import product from '@/app/products/[name]/page';
 
 
+
 export async function GET(request: Request) {
     let products
     const { MongoClient, ServerApiVersion } = require('mongodb');
-    const uri = `mongodb+srv://diespell:${process.env.MONGO_KEY}@cluster0.f1ogqle.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = process.env.MONGODB_URI;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     await client.connect();
     const collection = client.db("productsDb").collection("products");
